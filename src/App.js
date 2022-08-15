726ffdfe305d6876d3b9eb49ef33ff616db9b552
kiran <kiranpant9840@gmail.com>
2022-08-15 19:40:20 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App=() =>{
+  const arr = ['kiran', 'nabin', 'aone','niraj','diwas']
+  const filterPerson= arr.filter((item,index)=>{
+    if(item!='nabin'){
+      return item
+    }
+  })
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+   
+ <div id='header'>
+   
+    {
+      filterPerson.map((item,index)=>{
+      return <li>{item}</li>
+    })
+  }
+       
+
+    </div> 
   );
 }
 
